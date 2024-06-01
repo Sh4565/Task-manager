bb6dd7c4c18e7c68d56acf223b37f3d57139defc
Dmitriy <demon11343@gmail.com>
2024-06-01 18:19:26 +0300

    Tест вебхуков

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,10 +2,7 @@
 from django.contrib import admin
 from django.urls import path
 
-# from apps.TelegramBot.webhook import on_startup, on_shutdown
 
 urlpatterns = [
-    path('/', admin.site.urls),
-    # path('set-webhook/', on_startup, name='set_webhook'),
-    # path('delete-webhook/', on_shutdown, name='delete_webhook'),
+    path('/admin', admin.site.urls),
 ]
