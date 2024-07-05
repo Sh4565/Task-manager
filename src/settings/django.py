2e4d25212580a36a88cd8aa13b4159f72e284c21
Dmitriy <demon11343@gmail.com>
2024-07-05 17:14:31 +0300

    Переписал структуру кода

--- src/settings/django.py
+++ src/settings/django.py
@@ -14,7 +14,7 @@ DEBUG = env.bool('DEBUG')
 SECRET_KEY = env.str('SECRET_KEY')
 
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = env.str('ALLOWED_HOSTS', default='').split(' ')
 
 
 INSTALLED_APPS = [
